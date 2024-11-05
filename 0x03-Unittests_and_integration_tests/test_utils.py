52729eb992a02950d86995eed4d5144fad6f627b
eioforkansi <eioforkansi@gmail.com>
2024-11-05 16:56:09 +0100

    Task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,8 +4,11 @@ Unit test for utils.access_nested_map.
 """
 import unittest
 from parameterized import parameterized
+from unittest.mock import patch, Mock
 
 access_nested_map = __import__('utils').access_nested_map
+get_json = __import__('utils').get_json
+
 
 class TestAccessNestedMap(unittest.TestCase):
     """
@@ -29,3 +32,24 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test that a KeyError is raised for the following inputs"""
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
+
+class TestGetJson(unittest.TestCase):
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Test that utils.get_json returns the expected result"""
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+
+        self.assertEqual(result, test_payload)
+
