807ef1cd324822a22d424715a96051c486a07689
eioforkansi <eioforkansi@gmail.com>
2024-11-08 12:53:03 +0100

    Task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -69,7 +69,6 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-
         with patch.object(
             TestClass, 'a_method', return_value=lambda: 42,
         ) as mocked_method:
